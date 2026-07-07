1b5e71ec5703db35909822db6d25b93f5f51a982
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:13:05 +0530

    feat: Day 21 - Fortuner Class

---
+++ src/com/codegnan/oop/Fortuner.java
@@ -0,0 +1,27 @@
+package com.codegnan.oop;
+
+public class Fortuner implements Car1 {
+
+	@Override
+	public void turnDirection() {
+		
+	}
+
+	@Override
+	public void accelarate() {
+		
+	}
+
+	@Override
+	public void stop() {
+		
+	}
+
+	@Override
+	public void turnDirection(String dir) {
+		
+	}
+	
+	
+
+}
