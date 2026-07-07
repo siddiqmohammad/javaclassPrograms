68bd032bdc842e89d145fe4181e25ec943525706
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:05:48 +0530

    feat: Day 27 - User A Thread

---
+++ src/com/codegnan/multithreading/UserA.java
@@ -0,0 +1,17 @@
+package com.codegnan.multithreading;
+
+public class UserA implements Runnable{
+	BankAccount account;
+	public UserA (BankAccount account) {
+		this.account=account;
+	}
+
+	@Override
+	public void run() {
+		account.withdraw(Thread.currentThread().getName(),7000);
+		
+		
+	}
+	
+
+}
