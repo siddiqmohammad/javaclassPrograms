e994d318e4112223587343101f36568ca3b5ee0a
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:05:47 +0530

    feat: Day 27 - Runnable Demo

---
+++ src/com/codegnan/multithreading/MyRunnableDemo.java
@@ -0,0 +1,14 @@
+package com.codegnan.multithreading;
+
+public class MyRunnableDemo {
+	public static void main(String[] args) {
+		MyRunnable r = new MyRunnable();
+		Thread t = new Thread(r);
+		
+	
+	
+		}
+		
+	}
+
+
