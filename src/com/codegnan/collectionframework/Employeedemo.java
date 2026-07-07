1c0a1d5fc60a8cb02b25cb79f002c7fde1c91427
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 16:52:20 +0530

    feat: Day 29 - Employee Demo

---
+++ src/com/codegnan/collectionframework/Employeedemo.java
@@ -0,0 +1,11 @@
+package com.codegnan.collectionframework;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+public class Employeedemo {
+	public static void main(String[] args) {
+		
+
+	}
+
+}
