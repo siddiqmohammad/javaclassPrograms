75073d1ffd85452af60a009a0f8f7e52badc7446
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:13:06 +0530

    feat: Day 21 - Vehicle Demo

---
+++ src/com/codegnan/oop/VehicleDemo.java
@@ -0,0 +1,24 @@
+package com.codegnan.oop;
+
+public class VehicleDemo {
+	
+	public static void main(String[] args) {
+		Vehicle1 car =new Car2();
+		System.out.println("car operations");
+		car.accelarate();
+		car.accelarate();
+		car.accelarate();
+		car.accelarate();
+		car.accelarate();
+		car.stop();
+		Motorcycle bike = new Motorcycle();
+		bike.accelarate();
+		bike.accelarate();
+		bike.accelarate();
+		bike.accelarate();
+		bike.accelarate();
+		bike.stop();
+		
+	}
+
+}
