59d35a8bb268463538140b665f438824511cfec3
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:12:31 +0530

    feat: Day 17 - Employee Management Test

---
+++ src/com/codegnan/oop/Employee_01Test.java
@@ -0,0 +1,13 @@
+package com.codegnan.oop;
+
+public class Employee_01Test {
+
+	public static void main(String[] args) {
+			Employee_01 emp = new Employee_01(111,"mahesh",1000);
+			emp.id=101;
+			emp.name="siddu";
+			emp.salary=50000;
+			emp.displayDetails();
+		}
+		}
+
