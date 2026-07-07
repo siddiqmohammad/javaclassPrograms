4207b5f5ab831dc82ffffa8d11d978f042d6945a
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:13:26 +0530

    feat: Day 29 - OOP Practice Main Program

---
+++ src/com/codegnan/oop/Main1.java
@@ -0,0 +1,20 @@
+package com.codegnan.oop;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		Integer x=a; //autoboxing primitive to wrapper class object.
+		System.out.println("primitive value:" +a);
+		System.out.println("wrapper clsass integer X:" +x);
+		
+		
+		Long l=100l;
+		long y=l;
+		System.out.println("wrapper clsass Long value l:" +l);
+		System.out.println("primtive clsass long y:" +y);
+
+
+	}
+
+}
