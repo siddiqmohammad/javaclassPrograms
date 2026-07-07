39fda818be3da3e1bebb47462ae17f5390c4aa4e
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:03:04 +0530

    feat: Day 32 - Student Model Class

---
+++ src/com/codegnan/java8/Student.java
@@ -0,0 +1,14 @@
+package com.codegnan.java8;
+
+public class Student {
+	String name;
+	int marks;
+	
+public Student(String name, int marks) {
+		super();
+		this.name = name;
+		this.marks = marks;
+	}
+
+
+}
