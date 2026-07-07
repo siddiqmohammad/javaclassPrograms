f9f654c0d8a452bf669624944a16865263c6b173
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:02:56 +0530

    feat: Day 32 - Functional Interface Example

---
+++ src/com/codegnan/java8/Example.java
@@ -0,0 +1,16 @@
+package com.codegnan.java8;
+
+public class Example {
+public static void main(String[] args) {
+	Runnable r=()->{
+		for(int i=0;i<10;i++) {
+			System.out.println("child thread");
+		}
+	};
+	
+	Thread  t= new Thread(r);
+	for(int )
+}
+	
+	
+}
