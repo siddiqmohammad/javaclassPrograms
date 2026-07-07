aa25281d671699c8d311ff6b4fea0d3db72b7809
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:13:05 +0530

    feat: Day 21 - Password Checker

---
+++ src/com/codegnan/oop/Passwordchecker.java
@@ -0,0 +1,7 @@
+package com.codegnan.oop;
+
+public interface Passwordchecker {
+	public abstract String checkLength(String password);
+	public  abstract String checkComplexity(String password);
+
+}
