f7aa9821f8dd2eaab1e131db230d78270b32cbcc
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:13:06 +0530

    feat: Day 21 - Vehicle Example

---
+++ src/com/codegnan/oop/Vehicle1.java
@@ -0,0 +1,12 @@
+package com.codegnan.oop;
+
+public interface Vehicle1 {
+	int MAX_speed=200;
+	String FUEL_TYPE="Petrol";
+	void start();
+	void stop();
+	void accelarate();
+	void brake();
+	int getCurrentSpeed();
+
+}
