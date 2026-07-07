10ad110d2d6824ea5c0bb040d6bec242565d33a3
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 16:46:41 +0530

    feat: Day 13 - If Condition Example

---
+++ src/com/codegnan/fundamentals/Examplef_if.java
@@ -0,0 +1,14 @@
+package com.codegnan.fundamentals;
+
+public class Examplef_if {
+
+	public static void main(String[] args) {
+		int num = 10;
+
+		if (num > 0) {
+		    System.out.println("Positive Number");
+		}
+
+	}
+
+}
