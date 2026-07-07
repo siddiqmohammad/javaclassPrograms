be0347b3b7d84c8926ffe7f9777e588655a0efd8
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:12:53 +0530

    feat: Day 21 - Reverse String Program

---
+++ src/com/codegnan/oop/RevereseString.java
@@ -0,0 +1,12 @@
+package com.codegnan.oop;
+
+public class RevereseString {
+
+	public static void main(String[] args) {
+		StringBuilder sb =new StringBuilder("Codegnan");
+				sb.reverse();
+		System.out.println(sb);
+
+	}
+
+}
