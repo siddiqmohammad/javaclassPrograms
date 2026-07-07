e990ac0a621e308cad969360af8bd1633b608281
mohammad siddiq <siddimohammad770@gmail.com>
2026-07-07 17:05:47 +0530

    feat: Day 27 - Runnable Interface Example

---
+++ src/com/codegnan/multithreading/MyRunnable.java
@@ -0,0 +1,17 @@
+package com.codegnan.multithreading;
+
+public class MyRunnable  implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i=0; i<10;i++) {
+		System.out.println("seetha thread");
+			
+		try {
+			Thread.sleep(2000);
+			
+		}catch
+		}
+	}
+
+}
